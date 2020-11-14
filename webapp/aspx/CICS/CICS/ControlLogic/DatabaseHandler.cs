d28b34c5ceb0c31a447bb06a5b079cc374b5b9c2
nassersaazi <nassersnoop@gmail.com>
2020-11-14 06:41:43 +0300

    Addition of the database handling class.Removal of redundant pages

---
+++ webapp/aspx/CICS/CICS/ControlLogic/DatabaseHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace CICS.ControlLogic
+{
+    public class DatabaseHandler
+    {
+        private Database PegPayInterface;
+        private DbCommand mycommand;
+
+        public DatabaseHandler()
+        {
+            try
+            {
+
+                //PegPayInterface = DbLayer.CreateDatabase("LivePegPayConnectionString", DbLayer.DB2);
+                PegPayInterface = DatabaseFactory.CreateDatabase("LiveConnectionString");
+
+            }
+            catch (Exception up)
+            {
+                throw up;
+            }
+        }
+
+        public DataTable GetPendingPrepaidTransactions()
+        {
+            DataTable table = new DataTable();
+            try
+            {
+
+                mycommand = PegPayInterface.GetStoredProcCommand("GetPendingPrepaidTransactions5");
+                table = PegPayInterface.ExecuteDataSet(mycommand).Tables[0];
+                return table;
+            }
+            catch (Exception e)
+            {
+                //do nothing, return an empty table instead
+            }
+            return table;
+
+        }
+
+    }
+}
